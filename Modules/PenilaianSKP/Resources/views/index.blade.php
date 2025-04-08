10eaa927f87ad0cf517d4e89bcb25bcff2c038eb
widura26 <widurategalrejo@gmail.com>
2025-04-08 10:12:03 +0700

    create module

---
+++ Modules/PenilaianSKP/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('penilaianskp::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('penilaianskp.name') !!}
+    </p>
+@endsection
---
+++ Modules/PenilaianSKP/Resources/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Module PenilaianSKP</title>
+
+       {{-- Laravel Mix - CSS File --}}
+       {{-- <link rel="stylesheet" href="{{ mix('css/penilaianskp.css') }}"> --}}
+
+    </head>
+    <body>
+        @yield('content')
+
+        {{-- Laravel Mix - JS File --}}
+        {{-- <script src="{{ mix('js/penilaianskp.js') }}"></script> --}}
+    </body>
+</html>
